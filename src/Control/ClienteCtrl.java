41601ec34a4f6b16a45b08c60ce5a75a1957db1f
Dac <Dac@nome-10ef5775d8>
2016-05-22 12:29:01 -0300

    Inicial 9

---
+++ src/Control/ClienteCtrl.java
@@ -0,0 +1,26 @@
+/*
+ * Este Software tem Objetivo Educacional
+ * Para fins de aprendizagem e avaliacao na
+ * Na Disciplina de Programa��o Orientada a Objetos - Avan�ada
+ *  do Curso de Analise de Sistemas da Fatec - Ipiranga
+ * Ano 2016 - Janeiro a Junho 
+ * Aluno Decio Antonio de Carvalho  * 
+ */
+package Control;
+
+import java.sql.SQLException;
+import model.Cliente;
+import model.ClienteDAO;
+
+/**
+ *
+ * @author Dac
+ */
+public class ClienteCtrl {
+    
+     public void receberCliente(Cliente cliente) throws ClassNotFoundException, SQLException{
+        ClienteDAO cliDAO = new ClienteDAO();
+        cliDAO.inserirNovoCliente(cliente);
+    }
+    
+}
