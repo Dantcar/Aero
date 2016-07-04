7cc0ee49c2de7c36962fb996ab8f499a38a000d0
deciodecarvalho <deciodecarvalho@05S006475-187.seade.net>
2016-07-04 13:52:03 -0300

    Ajustes nos comboBox dos relatórios para somente habilitarem os botões respectivos de busca, quando existir algum valor.

---
+++ src/model/AssentoAeronave.java
@@ -0,0 +1,17 @@
+/*
+ * Este Software tem Objetivo Educacional
+ * Para fins de aprendizagem e avaliacao na
+ * Na Disciplina de Programa��o Orientada a Objetos - Avan�ada
+ *  do Curso de Analise de Sistemas da Fatec - Ipiranga
+ * Ano 2016 - Janeiro a Junho 
+ * Aluno Decio Antonio de Carvalho  * 
+ */
+package model;
+
+/**
+ *
+ * @author deciodecarvalho
+ */
+public class AssentoAeronave {
+    
+}
